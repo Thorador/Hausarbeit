a9b0ad0a07b20b8ce8b466787e53327b081ab8bb
flydown <kevin.hesmer@gmail.com>
2017-03-22 11:05:18 +0100

    mein Profil

---
+++ src/main/java/view/MeinProfilView.java
@@ -0,0 +1,156 @@
+package view;
+
+import java.util.Date;
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import model.User;
+import service.UserService;
+import service.SessionService;
+
+@Named
+@SessionScoped
+public class MeinProfilView implements java.io.Serializable{
+
+	private String benutzername;
+	private String passwort;
+	private String passwortbestaetigen;
+	private String vorname;
+	private String nachname;
+	private Date geburtsdatum;
+	private String rolle;
+	private String geschlecht;
+	private String strasse;
+	private String ort;
+	private int plz;
+	
+	@Inject
+	private UserService userService;
+	
+	@Inject
+	private SessionService sessionService;
+	
+	public MeinProfilView()
+	{
+		
+	}
+	
+	public String importBenutzer()
+	{// Füllen der Werte auf der Benutzeroberfäche
+		if (sessionService != null && sessionService.isLoggedIn())
+		{
+			User user = sessionService.getActiveUser();
+			setBenutzername(user.getBenutzername());
+			setVorname(user.getVorname());
+			setNachname(user.getNachname());
+			setGeburtsdatum(user.getGeburtsdatum());
+			setGeschlecht(user.getGeschlecht());
+			setStrasse(user.getStrasse());
+			setOrt(user.getOrt());
+			setPlz(user.getPlz());
+		}else
+		{
+			setBenutzername("Kein Benutzer eingeloggt!");
+			setVorname("");
+			setNachname("");
+			setGeburtsdatum(null);
+			setGeschlecht("");
+			setStrasse("");
+			setOrt("");
+			setPlz(0);
+		}
+		return "meinProfil.jsf";
+	}
+	
+	public String save()
+	{
+		sessionService.getActiveUser().setVorname(getVorname());
+		sessionService.getActiveUser().setNachname(getNachname());
+		sessionService.getActiveUser().setGeburtsdatum(getGeburtsdatum());
+		sessionService.getActiveUser().setGeschlecht(getGeschlecht());
+		sessionService.getActiveUser().setStrasse(getStrasse());
+		sessionService.getActiveUser().setOrt(getOrt());
+		sessionService.getActiveUser().setPlz(getPlz());
+		
+		return "frontpage.jsf";
+	}
+
+	public String cancel()
+	{
+		return "frontpage.jsf";
+	}
+	
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public Date getGeburtsdatum() {
+		return geburtsdatum;
+	}
+	public void setGeburtsdatum(Date geburtsdatum) {
+		this.geburtsdatum = geburtsdatum;
+	}
+	public String getRolle() {
+		return rolle;
+	}
+	public void setRolle(String rolle) {
+		this.rolle = rolle;
+	}
+	public String getGeschlecht() {
+		return geschlecht;
+	}
+	public void setGeschlecht(String geschlecht) {
+		this.geschlecht = geschlecht;
+	}
+	public String getStrasse() {
+		return strasse;
+	}
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+	public String getOrt() {
+		return ort;
+	}
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+	public int getPlz() {
+		return plz;
+	}
+	public void setPlz(int plz) {
+		this.plz = plz;
+	}
+	public String getBenutzername() {
+		return benutzername;
+	}
+	public void setBenutzername(String benutzername) {
+		this.benutzername = benutzername;
+	}
+	public String getPasswort() {
+		return passwort;
+	}
+	public void setPasswort(String passwort) {
+		this.passwort = passwort;
+	}
+	public String getPasswortbestaetigen() {
+		return passwortbestaetigen;
+	}
+	public void setPasswortbestaetigen(String passwortbestaetigen) {
+		this.passwortbestaetigen = passwortbestaetigen;
+	}
+}
